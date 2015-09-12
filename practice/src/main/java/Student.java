f2a9389955928d6607dcb50473a84df7e39db5cb
almapratheek <almapratheekreddy@yahoo.com>
2015-09-12 01:25:22 -0700

    added Student class

---
+++ practice/src/main/java/Student.java
@@ -0,0 +1,60 @@
+package main.java;
+
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Student {
+    private int rollno;
+    private String name;
+    private int age;
+    private int totalMarks;
+
+    Student(int rollno, String name, int age, int totalMarks) {
+        this.rollno = rollno;
+        this.name = name;
+        this.age = age;
+        this.totalMarks = totalMarks;
+    }
+
+    public static void sortAndPrint(List<Student> al, int age, Comparator<Student> c) {
+        al.stream()
+                .sorted(c)
+                .forEach((a) -> {
+                    if (a.getAge() < age)
+                        System.out.println(a.getName() + "\t" + a.getAge() + "\t" + a.getRollno() + "\t" + a.getTotalMarks());
+                });
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTotalMarks() {
+        return totalMarks;
+    }
+
+    public void setTotalMarks(int totalMarks) {
+        this.totalMarks = totalMarks;
+    }
+}
